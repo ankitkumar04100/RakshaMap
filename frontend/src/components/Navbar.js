ad3599d97fce8c3eb732f480dff5abbd80d89f23
ankitkumar04100 <ankitkumarforall@gmail.com>
2026-02-07 13:28:08 +0530

    Create Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>RakshaMap</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
